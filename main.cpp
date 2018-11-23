2cf390f1d641b6ef80bc86aa89ad04314b9f5391
Wasek Rahman <31708476+WasekRahman@users.noreply.github.com>
2018-11-23 23:03:58 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,12 +15,17 @@ int subOperation(int a,int b)
 {
     return a-b;
 }
+int multOperation(int a,int b)
+{
+    return a*b;
+}
+
 
 void printBinary(string a,int b, int c, int d)
 {
-    bitset<2> BinaryStore(d);
-    bitset<2> BinaryRs(b);
-    bitset<2> BinaryRt(c);
+    bitset<5> BinaryStore(d);
+    bitset<5> BinaryRs(b);
+    bitset<5> BinaryRt(c);
     cout<<a<<" "<<BinaryRs<<" "<<BinaryRt<<" "<<BinaryStore<<endl;
     cout<<endl;
 
@@ -28,18 +33,20 @@ void printBinary(string a,int b, int c, int d)
 
 int main()
 {
-    std::vector<std::string> vecOfStr;
+    std::vector<std::string> memo;
     std::vector<std::string> word;
     std::vector<std::string> reg;
     string testline;
     string tempStorage;
     int temp[4];
-    int temp1[4];
     string add = "ADD";
     string sub = "SUB";
+    string mult = "MULT";
+    string lw = "lw";
     string tempString[20];
     string opCode;
-    int store;
+    int store,locate;
+
 
     ifstream assemblyCode ("instructions.txt" );
 
@@ -70,6 +77,17 @@ int main()
     {
         reg.push_back(testline);
     }
+    ifstream memory("memory.txt");
+    if (!memory)
+    {
+        cout << "There was an error opening the file.\n";
+        return 0;
+    }
+
+    while( memory>>testline )
+    {
+        memo.push_back(testline);
+    }
 
 
 
@@ -93,7 +111,8 @@ int main()
                     tempString[j-2] = word[i+j];
 
                 }
-                else if(temp[j-2]!=0){
+                else if(temp[j-2]!=0)
+                {
                     tempString[j-2]=word[i+j];
                 }
 
@@ -102,38 +121,39 @@ int main()
 
             for (int i=0; i<reg.size(); i++)
             {
-                if(temp[0]==0 || temp[1]==0 ){
-                if(reg[i]==tempString[0] && reg[i]==tempString[1])
+                if(temp[0]==0 || temp[1]==0 )
                 {
-                    string s = reg[i+1];
-                    stringstream value(s);
-                    int tempReg = 0;
-                    value >> tempReg;
-                    temp[0]=tempReg;
-                    temp[1]=tempReg;
+                    if(reg[i]==tempString[0] && reg[i]==tempString[1])
+                    {
+                        string s = reg[i+1];
+                        stringstream value(s);
+                        int tempReg = 0;
+                        value >> tempReg;
+                        temp[0]=tempReg;
+                        temp[1]=tempReg;
 
 
-                }
-                else if(reg[i] == tempString[0])
-                {
+                    }
+                    else if(reg[i] == tempString[0])
+                    {
 
-                    string s = reg[i+1];
-                    stringstream value(s);
-                    int tempReg = 0;
-                    value >> tempReg;
-                    temp[0]=tempReg;
-                }
+                        string s = reg[i+1];
+                        stringstream value(s);
+                        int tempReg = 0;
+                        value >> tempReg;
+                        temp[0]=tempReg;
+                    }
 
-                else if
-                (reg[i] == tempString[1])
-                {
+                    else if
+                    (reg[i] == tempString[1])
+                    {
 
-                    string s = reg[i+1];
-                    stringstream value(s);
-                    int tempReg = 0;
-                    value >> tempReg;
-                    temp[1]=tempReg;
-                }
+                        string s = reg[i+1];
+                        stringstream value(s);
+                        int tempReg = 0;
+                        value >> tempReg;
+                        temp[1]=tempReg;
+                    }
                 }
 
                 else if(reg[i] == add)
@@ -165,7 +185,8 @@ int main()
                     tempString[j-2] = word[i+j];
 
                 }
-                else if(temp[j-2]!=0){
+                else if(temp[j-2]!=0)
+                {
                     tempString[j-2]=word[i+j];
                 }
 
@@ -173,40 +194,41 @@ int main()
 
             for (int i=0; i<reg.size(); i++)
             {
-                if(temp[0]==0 || temp[1]==0 ){
-                if(reg[i]==tempString[0] && reg[i]==tempString[1])
+                if(temp[0]==0 || temp[1]==0 )
                 {
-                    string s = reg[i+1];
-                    stringstream value(s);
-                    int tempReg = 0;
-                    value >> tempReg;
-                    temp[0]=tempReg;
-                    temp[1]=tempReg;
+                    if(reg[i]==tempString[0] && reg[i]==tempString[1])
+                    {
+                        string s = reg[i+1];
+                        stringstream value(s);
+                        int tempReg = 0;
+                        value >> tempReg;
+                        temp[0]=tempReg;
+                        temp[1]=tempReg;
 
 
-                }
+                    }
 
-                else if(reg[i] == tempString[0])
-                {
+                    else if(reg[i] == tempString[0])
+                    {
 
-                    string s = reg[i+1];
-                    stringstream value(s);
-                    int tempReg = 0;
-                    value >> tempReg;
-                    temp[0]=tempReg;
+                        string s = reg[i+1];
+                        stringstream value(s);
+                        int tempReg = 0;
+                        value >> tempReg;
+                        temp[0]=tempReg;
 
-                }
-                else if
-                (reg[i] == tempString[1])
-                {
+                    }
+                    else if
+                    (reg[i] == tempString[1])
+                    {
 
-                    string s = reg[i+1];
-                    stringstream value(s);
-                    int tempReg = 0;
-                    value >> tempReg;
-                    temp[1]=tempReg;
+                        string s = reg[i+1];
+                        stringstream value(s);
+                        int tempReg = 0;
+                        value >> tempReg;
+                        temp[1]=tempReg;
 
-                }
+                    }
                 }
 
                 else if
@@ -224,8 +246,162 @@ int main()
             int subtracted = subOperation(temp[0],temp[1]);
             printBinary(opCode,temp[0],temp[1],subtracted);
         }
-    }
+        else if(word[i]==mult)
+        {
+            cout<<word[i]<<" "<<word[i+1]<<" , "<<word[i+2]<<" , "<<word[i+3]<<endl;
 
+            for(int j=2; j<=3; j++)
+
+            {
+                string s = word[i+j];
+                stringstream value(s);
+                int tempValue = 0;
+                value >> tempValue;
+                temp[j-2]=tempValue;
+                if(temp[j-2]==0)
+                {
+                    tempString[j-2] = word[i+j];
+
+                }
+                else if(temp[j-2]!=0)
+                {
+                    tempString[j-2]=word[i+j];
+                }
+
+            }
+
+            for (int i=0; i<reg.size(); i++)
+            {
+                if(temp[0]==0 || temp[1]==0 )
+                {
+                    if(reg[i]==tempString[0] && reg[i]==tempString[1])
+                    {
+                        string s = reg[i+1];
+                        stringstream value(s);
+                        int tempReg = 0;
+                        value >> tempReg;
+                        temp[0]=tempReg;
+                        temp[1]=tempReg;
+
+
+                    }
+
+                    else if(reg[i] == tempString[0])
+                    {
+
+                        string s = reg[i+1];
+                        stringstream value(s);
+                        int tempReg = 0;
+                        value >> tempReg;
+                        temp[0]=tempReg;
+
+                    }
+                    else if
+                    (reg[i] == tempString[1])
+                    {
+
+                        string s = reg[i+1];
+                        stringstream value(s);
+                        int tempReg = 0;
+                        value >> tempReg;
+                        temp[1]=tempReg;
+
+                    }
+                }
+
+                else if
+                (reg[i] == mult)
+                {
+
+                    opCode = reg[i+1];
+                }
+
+
+
+            }
+
+
+            int multiplied = multOperation(temp[0],temp[1]);
+            printBinary(opCode,temp[0],temp[1],multiplied);
+        }
+        else if(word[i]==lw)
+        {
+            cout<<word[i]<<" "<<word[i+1]<<" , "<<word[i+2]<<" , "<<word[i+3]<<endl;
+
+            for(int j=2; j<=3; j++)
+
+            {
+                string s = word[i+j];
+                stringstream value(s);
+                int tempValue = 0;
+                value >> tempValue;
+                temp[j-2]=tempValue;
+                if(temp[j-2]==0)
+                {
+                    tempString[j-2] = word[i+j];
+
+                }
+                else if(temp[j-2]!=0)
+                {
+                    tempString[j-2]=word[i+j];
+                }
+
+            }
+
+            for (int i=0; i<reg.size(); i++)
+            {
+
+                if
+                (reg[i] == tempString[1])
+                {
+                    string s = reg[i+1];
+                    stringstream value(s);
+                    int tempReg = 0;
+                    value >> tempReg;
+                    temp[1]=tempReg;
+                    if(temp[1]==0)
+                    {
+                        for(int i=0; i<=memo.size(); i++)
+                        {
+                            if(memo[i]==s)
+                            {
+                                string s = memo[i+1];
+                                stringstream value(s);
+                                int tempReg = 0;
+                                value >> tempReg;
+                                temp[1]=tempReg;
+                                int locate = i+1 + (temp[0]*2);
+                                string t = memo[locate];
+                                stringstream value2(t);
+                                int tempLocate = 0;
+                                value2 >> tempLocate;
+                                temp[2]=tempLocate;
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        temp[1]=tempReg;
+                    }
+
+                }
+
+
+                else if
+                (reg[i] == lw)
+                {
+
+                    opCode = reg[i+1];
+                }
+
+            }
+            printBinary(opCode,temp[0],temp[1],temp[2]);
+        }
+
+    }
+    memory.close();
+    registers.close();
     assemblyCode.close();
 
 
